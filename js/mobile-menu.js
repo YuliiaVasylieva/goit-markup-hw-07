1b32575dbc613303621b68e715b3f9c74c0a85a5
YuliiaVasylieva <117345448+YuliiaVasylieva@users.noreply.github.com>
2023-01-02 12:23:19 +0200

    add

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,18 +1,13 @@
 (() => {
-  const menuContainer = document.querySelector(".js-menu-container");
-  const openMenuBtn = document.querySelector(".js-open-menu");
-  const closeMenuBtn = document.querySelector(".js-close-menu");
-
-  const toggleMenu = () => menuContainer.classList.toggle("is-open");
-  openMenuBtn.addEventListener("click", toggleMenu);
-  closeMenuBtn.addEventListener("click", toggleMenu);
-
-  // Закрываем мобильное меню на более широких экранах
-  // в случае изменения ориентации устройства.
-  window
-    .matchMedia("(min-width: 600px)")
-    .addEventListener(
-      "change",
-      (e) => e.matches && menuContainer.classList.remove("is-open")
-    );
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+  };
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  function toggleMenu() {
+    document.body.classList.toggle("menu-open");
+    refs.menu.classList.toggle("is-open");
+  }
 })();
